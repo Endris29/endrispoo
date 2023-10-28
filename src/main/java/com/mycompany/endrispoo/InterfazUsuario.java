8ba3043d21b7504a76fed0a507fa3637ef8dc48f
Endris <endrissanchez12@gmail.com>
2023-10-28 18:14:13 -0500

    Commit 3

--- src/main/java/com/mycompany/endrispoo/InterfazUsuario.java
+++ src/main/java/com/mycompany/endrispoo/InterfazUsuario.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.endrispoo;
+import java.util.Scanner;
 
-/**
- *
- * @author LENOVO
- */
 public class InterfazUsuario {
-    
+
+    private Scanner scanner;
+
+    public InterfazUsuario() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void mostrarPregunta(String pregunta) {
+        System.out.println(pregunta);
+    }
+
+    public int obtenerRespuestaUsuario() {
+        return scanner.nextInt();
+    }
 }
+
